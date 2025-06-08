1964333c98a3cb202044be8969c9c2ae7002de28
Vaibhav MS <vaibhavsingh4141@gmail.com>
2025-06-08 15:04:45 +0530

    Added backend CI/CD pipeline

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   eslint:{
     ignoreDuringBuilds:true
+  },
+  images: {
+    unoptimized: false,
+    domains: [], 
   },
   transpilePackages: [
     "@repo/ui"
