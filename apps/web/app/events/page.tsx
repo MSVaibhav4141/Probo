8c79655aade8406690cb06ca0e1c08d66e00e016
Vaibhav M Singh <106318667+MSVaibhav4141@users.noreply.github.com>
2026-03-25 03:54:54 +0530

    Update page.tsx
    Making it dynamic

--- apps/web/app/events/page.tsx
+++ apps/web/app/events/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import EventCard from "@components/events/EventCard";
 import Tabs from "@components/events/EventTab";
 import { getServerSession } from "next-auth";
